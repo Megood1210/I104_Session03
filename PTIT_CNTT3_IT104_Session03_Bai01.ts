6e3e792704839409c1f182d7bef6934fbfda0e3b
Megood1210 <xuanvinhle06@gmail.com>
2025-08-06 20:48:40 +0700

    Upload bài tập 1

---
+++ PTIT_CNTT3_IT104_Session03_Bai01.ts
@@ -0,0 +1,13 @@
+let username: string = "Le Hoang Xuan Vinh";
+let age: number = 25;
+let job: string = "Developer";
+
+// Định nghĩa hàm in thông tin người dùng
+function printUserInfo(name: string, age: number, job: string): void {
+    console.log("Tên:", name);
+    console.log("Tuổi:", age);
+    console.log("Nghề nghiệp:", job);
+}
+
+
+printUserInfo(username, age, job);
