78e92f78345fbc628e33b3e29cdd996abed84715
Megood1210 <xuanvinhle06@gmail.com>
2025-08-06 20:49:08 +0700

    Upload bài tập 2

---
+++ PTIT_CNTT3_IT104_Session03_Bai02.ts
@@ -0,0 +1,7 @@
+let username: string = "Alice";  
+username = 123;  
+//Có lỗi xảy ra vì 
+// -Biến username đã được khai báo là kiểu string. 
+//  -Sau đó bạn lại gán một giá trị kiểu number (123) cho biến này.
+//nên nó vi phạm quy tắc kiểu dữ liệu mà TypeScript đã áp đặt.
+
